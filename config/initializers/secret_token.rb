b5628a166cd7db5c6ba8271626467df4f07f9058
Derek Cannon <derek.cannon@sage.com>
2012-11-01 20:36:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperWhiteboard::Application.config.secret_token = 'ca93490a139aba5e2132cb585d8eede1517104bea7bedc62916f7cf9d374e1642726f668f17c7b70d865733ba698666db108b31aabbacfa12f32e984216e1287'
