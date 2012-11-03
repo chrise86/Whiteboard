a784f83d0ca5af68f4e73f8ff0f941363c97d2eb
Derek Cannon <derek@derekcannon.com>
2012-11-03 16:34:18 -0400

    Added associations for the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+	# Include default devise modules. Others available are:
+	# :token_authenticatable, :confirmable,
+	# :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+		:recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :first_name, :last_name
-  # attr_accessible :title, :body
+	# Setup accessible (or protected) attributes for your model
+	attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :first_name, :last_name
+	# attr_accessible :title, :body
+
+	has_many :professor_events
+	has_many :user_sections
+	has_many :user_responses
+	has_many :gradebooks
+	has_many :categories # e.g. Professor has categories for Events ("Chapter 1", "Chapter 2", etc.)
+	
+	has_one :calendar_preferences
 end
